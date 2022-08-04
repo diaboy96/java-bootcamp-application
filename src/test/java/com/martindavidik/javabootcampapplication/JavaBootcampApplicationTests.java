8484d4979ae3b178453d0a75390931d0c65feb05
Martin Davidík <martin.davidik@lundegaard.net>
2022-08-04 11:03:36 +0200

    Initial commit

---
+++ src/test/java/com/martindavidik/javabootcampapplication/JavaBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martindavidik.javabootcampapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
